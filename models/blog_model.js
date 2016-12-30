4070496f1584b5ee4e9b541d0cd4b60c2eca9cb4
Darshan <Darshan>
2016-12-30 15:59:25 +0530

    db

--- models/blog_model.js
+++ models/blog_model.js
@@ -13,7 +13,7 @@ return db.query("select * from blog_tbl where blog_id=?",[id],callback);
  },
 
  addBlog:function(Blog,callback){
- return db.query("Insert into blog_tbl values(?,?,?,?,?,?,?,?,?)",[Blog.blog_id,Blog.blog_title,Blog.blog_desc,Blog.blog_date,Blog.blog_time,Blog.blog_photo,Blog.view,Blog.fk_u_email_id,Blog.fk_sub_id],callback);
+ return db.query("Insert into blog_tbl values(?,?,?,?,?,?,?,?,?,?)",[Blog.blog_id,Blog.blog_title,Blog.blog_desc,Blog.blog_date,Blog.blog_time,Blog.blog_photo,Blog.flag,Blog.view,Blog.fk_u_email_id,Blog.fk_sub_id],callback);
  },
 
 deleteBlog:function(id,callback){
