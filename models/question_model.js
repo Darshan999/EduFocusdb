0822483afd0143be1621588e0794e8c9cf9e2690
Darshan <Darshan>
2017-01-16 18:33:03 +0530

    don3

--- models/question_model.js
+++ models/question_model.js
@@ -1,5 +1,5 @@
 var db=require('../dbconnection'); //reference of dbconnection.js
- 
+ var fs = require('fs');
 var Question={
 
 getAllQuestion:function(callback){
@@ -19,7 +19,29 @@ return db.query("Select q.*,s.*,u.* from question_tbl as q,subject_tbl as s,user
 
 
  addQuestion:function(Question,callback){
- return db.query("Insert into question_tbl values(?,?,?,?,?,?,?,?,?,?)",[Question.que_id,Question.que_title,Question.que_desc,Question.que_date,Question.que_time,Question.que_photo,Question.flag,Question.view,Question.fk_u_email_id,Question.fk_sub_id],callback);
+
+     
+       var dt=new Date();//current date and time of server
+    var text = "";//random text
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    //var base64d=property.pro_image.replace(/^data:image\/png;base64,/, "");
+
+   // base64d=property.pro_image.replace(/^data:image\/jpg;base64,/, "");
+    // base64d=property.pro_image.replace(/^data:image\/jpeg;base64,/, "");
+    var pos=Question.que_photo.indexOf(",");
+    var base64d=Question.que_photo.substring(pos+1);
+    console.log(base64d);
+    var path="./public/images/question/"+text+dt.getDate()+dt.getMonth()+dt.getMilliseconds()+".png";
+    var path1="/images/question/"+text+dt.getDate()+dt.getMonth()+dt.getMilliseconds()+".png";
+    fs.writeFile(path,base64d,'base64',function(err){
+        if(err) {
+        return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+ return db.query("Insert into question_tbl values(?,?,?,?,?,?,?,?,?,?)",[Question.que_id,Question.que_title,Question.que_desc,Question.que_date,Question.que_time,path1,Question.flag,Question.view,Question.fk_u_email_id,Question.fk_sub_id],callback);
  },
 
 deleteQuestion:function(id,callback){
